970a615d74fff16f9fc11311902bb48bd5b45095
dichha <dichha-rai@uiowa.edu>
2017-08-17 23:16:59 -0500

    changed how prefix is evaluated * returns prefix at the end of while loop after finding low and high indices * checks if all prefix is contained using java's `startsWith` method

--- LCP.java
+++ LCP.java
@@ -3,13 +3,11 @@ import java.lang.Integer;
 // Longest Common Prefix using Binary Search 
 
 public class LCP{
-	private boolean allContainsPrefix(String[] arr, String str,  int start, int end){
+	private boolean allContainsPrefix(String[] arr, int len){
+		String str = arr[0].substring(0, len);
 		for (int i=0; i<arr.length; i++){
-			for(int j=start; j<=end; j++){
-				if(arr[i].charAt(j) != str.charAt(j)){	
-					return false;
-				}
-			}
+			if(!arr[i].startsWith(str))
+				return false;
 		}
 		return true; 
 	}
@@ -26,26 +24,22 @@ public class LCP{
 	private String commonPrefix(String[] arr){
 		int len = findMinLength(arr);
 		int low = 0, high = len;
-
-		String prefix = ""; 
 		while(low <= high){
 			// Same as (low+high)/2, but avoids overflow for large low and high
 			int mid = low + (high-low)/2;
-			if(allContainsPrefix(arr, arr[0], low, mid)){
-				prefix = prefix + arr[0].substring(low, mid-low+1);
+			if(allContainsPrefix(arr, mid)){
 				low = mid+1;
 			}else{
 				high = mid-1;
 			}
 
 		}
-		return prefix;
+		return arr[0].substring(0, (low+high)/2);
 	}
 	public static void main(String[] args){
-		String[] arr = {"geeksforgeeks", "geeks", "geek", "geezer"};
+		String[] arr = {"aerospace", "aerodynamics", "aeroplane", "aeroengineering", "aeol"};
 		LCP lcp = new LCP();
 		String ans = lcp.commonPrefix(arr);
-		System.out.println("The longest common prefix is " + ans);
-		
+		System.out.println("Answer is: " + ans);
 	}
 }
