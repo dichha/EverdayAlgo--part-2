625d14409dd6fd25b8c6505c8f694e9dba99f4dc
dichha <dichha-rai@uiowa.edu>
2017-08-20 00:08:58 -0500

    renamed a file name * The algorithm actually deletes all presence of a given value and not duplicates, therefore removed `Duplicates` from the file name

---
+++ RemoveGivenValue.java
@@ -0,0 +1,22 @@
+public class RemoveGivenValue{
+	public int removeElement(int[] nums, int val){
+		int b = 0;
+		int c = 0;
+		for(int i=0; i<nums.length; i++){
+			if(nums[i] != val){
+				nums[b] = nums[i];
+				b++;
+				c++;
+			}
+		} 
+		for(int j=0; j<b; j++){
+			System.out.print(nums[j] + " ");
+		}
+		return c;
+	}
+	public static void main(String[] args){
+		RemoveDuplicateGivenValue rdgv = new RemoveDuplicateGivenValue();
+		int ans = rdgv.removeElement(new int[]{1,1,3,5,6,5}, 5);
+		System.out.println("Ans is: " + ans);
+	}
+}
